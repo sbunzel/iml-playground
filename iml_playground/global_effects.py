13df380f0b6e6032e81bd12978400af205c432bc
Steffen Bunzel <steffenbunzel@posteo.de>
2021-04-15 22:38:15 +0200

    Clean up and add comments

--- iml_playground/global_effects.py
+++ iml_playground/global_effects.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 import altair as alt
+import numpy as np
 import pandas as pd
 from sklearn import inspection
 
@@ -12,9 +13,11 @@ class GlobalEffects:
     def __init__(self, model: Model, method: str, feature: str) -> None:
         self.model = model
         self.feature = feature
-        X = model.X_test.copy()
-        self.X = X.loc[
-            X[feature].between(X[feature].quantile(0.05), X[feature].quantile(0.95))
+        self.X = model.X_test.loc[
+            model.X_test[feature].between(
+                model.X_test[feature].quantile(0.05),
+                model.X_test[feature].quantile(0.95),
+            )
         ]
         methods = {
             "partial_dependence_plot": self._calculate_partial_dependence,
@@ -27,17 +30,20 @@ class GlobalEffects:
                 f"'{method}' is not a valid method. Possible options are {list(methods.keys())}"
             )
 
-    def _calculate_partial_dependence(self, feature: str):
-        res = inspection.partial_dependence(
+    def _calculate_partial_dependence(
+        self, feature: str
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        pd = inspection.partial_dependence(
             estimator=self.model.estimator,
             X=self.X,
             features=[feature],
             kind="average",
             percentiles=(0, 1),
         )
-        return res["average"], res["values"][0]
+        effects, values = pd["average"].reshape(-1), pd["values"][0]
+        return effects, values
 
-    def _calculate_ale(self, feature: str):
+    def _calculate_ale(self, feature: str) -> Tuple[np.ndarray, np.ndarray]:
         effects, quantiles = _first_order_ale_quant(
             self.model.estimator.predict,
             self.X,
@@ -48,9 +54,10 @@ class GlobalEffects:
         return effects, values
 
     def plot(self, title_config: Dict[str, Any]) -> alt.Chart:
+        # Line plot of average effects
         effects_df = pd.DataFrame(
             {
-                "Average Effect": self.avg_effect.reshape(-1),
+                "Average Effect": self.avg_effect,
                 self.feature: self.values,
             }
         )
@@ -62,10 +69,11 @@ class GlobalEffects:
                 y=alt.Y("Average Effect:Q"),
             )
         )
-
+        # Histogram of feature values
         min_value, max_value = (self.values.min(), self.values.max())
-        values_df = self.model.X_test[[self.feature]]
-        values_df = values_df.loc[values_df[self.feature].between(min_value, max_value)]
+        values_df = self.model.X_test[[self.feature]].loc[
+            self.model.X_test[self.feature].between(min_value, max_value)
+        ]
         step_size = (max_value - min_value) / 10
         values_hist = (
             alt.Chart(values_df)
@@ -75,6 +83,7 @@ class GlobalEffects:
                 y=alt.Y("count()", title="Number of Samples"),
             )
         )
+        # Layered combination of line and histogram
         chart = values_hist + effect_line
         return (
             chart.properties(
